5d22c220812913459d9714da077c3dfcb1b17d76
wasi97 <mw7262@gmail.com>
2024-03-09 10:10:43 +0500

    auth Gaurd updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,21 @@ import { Route, Routes } from 'react-router-dom'
 import Login from './pages/Login'
 import SignUp from './pages/SignUp'
 import Home from './pages/Home'
+import PublicRoutes from './config/Routes/PublicRoutes'
+import ProtectedRoutes from './config/Routes/ProtectedRoutes'
 
 function App() {
 
   return (
    <>
    <Routes>
+    <Route element={<PublicRoutes />}>
     <Route index element={<Login />} />
     <Route path='/signup' element={<SignUp />} />
+    </Route>
+    <Route element={<ProtectedRoutes />}>
     <Route path='/home' element={<Home />} />
+    </Route>
    </Routes>
    </>
   )
--- src/config/Routes/PublicRoutes.jsx
+++ src/config/Routes/PublicRoutes.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
 const PublicRoutes = () => {
-  return localStorage.getItem("uid") ? <Outlet /> : <Navigate to={"/"} />;
+  return !localStorage.getItem("uid") ? <Outlet /> : <Navigate to={"/"} />;
 };
 
 export default PublicRoutes;
